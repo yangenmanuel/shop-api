e0921ffc9982c0cf4d78e8cbb9cadf944d81a4b7
Yang Enmanuel <enmanuelcalzat@gmail.com>
2024-01-24 21:59:59 -0300

    Anadido comentarios

--- src/index.js
+++ src/index.js
@@ -10,16 +10,21 @@ app.get("/", (req, res) => {
   res.json("Hello, world!");
 });
 
+// Todos los items
 app.get("/items", (req, res) => {
   res.json(store.items);
 });
 
+
+// Un item por el id ex: item/1
 app.get("/items/:id", (req, res) => {
   const { id } = req.params;
   const item = store.items.find((i) => i.id == id);
   res.json(item);
 });
 
+
+// Añadir un item ex: item?name=earbuds&?description=expensiveShit
 app.post("/items", (req, res) => {
   const { name, description } = req.query;
   const id = store.items.length + 1;
@@ -30,6 +35,7 @@ app.post("/items", (req, res) => {
   res.send(store)
 })
 
+// Borrar un item por el id
 app.delete("/items/:id", (req, res) => {
   const { id } = req.params;
   store.items = store.items.filter((i) => i.id != id);
